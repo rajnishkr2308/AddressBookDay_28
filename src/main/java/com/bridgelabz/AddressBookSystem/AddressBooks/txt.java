5ac5c0358a03e3fb8169c47ab9b6051ac847fbec
rajnishkr2308 <raj123rajnish2308@gmail.com>
2023-05-25 22:38:36 +0530

    Added:WelcomeToAddressBookWithPersonContactascsv

---
+++ src/main/java/com/bridgelabz/AddressBookSystem/AddressBooks/txt.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.AddressBookSystem.AddressBooks;
+
+class AddressBook{
+
+    String Contacts;
+    char firstName;
+     char lastName;
+    char address;
+    char city;
+    char state;
+    char zip;
+
+    char phoneNumber;
+
+    }
